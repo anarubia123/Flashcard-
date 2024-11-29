3aa03b0034ccd89b0103c801f14869d0518455de
anarubia123 <anarubiasantos550@gmail.com>
2024-11-29 12:11:59 -0300

    Projeto Final Flash Card

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Português',
+    'O que é leitura?',
+    'Leitura é o processo de interpretar palavras e frases escritas para entender o seu significado.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Matemática',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
